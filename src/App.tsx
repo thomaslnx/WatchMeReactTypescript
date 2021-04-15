94ecea824ee848f75fd4ebf19b5a41a1b14d0db0
thomaslnx <marcos.msilva10@gmail.com>
2021-04-15 17:39:36 -0300

    Component content complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { useContext } from 'react';
-import { MoviesCategoriesContext } from './MoviesCategoriesContext';
-import { MovieCard } from './components/MovieCard';
-
 import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content } from './components/Content';
 
 import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
 export function App() {
-  const { genre, genreId, genreSelected, movie } = useContext(MoviesCategoriesContext);
-
-  const [ selectedGenre, setSelectedGenre ] = genreSelected;
-  const [ movies, setMovies ] = movie;
-  
   return (
       <div style={{ display: 'flex', flexDirection: 'row' }}>
-
         <SideBar />
-
-        <div className="container">
-          <header>
-            <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-          </header>
-
-          <main>
-            <div className="movies-list">
-              {movies.map(movie => (
-                <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-                ))}
-            </div>
-          </main>
-        </div>
+        <Content />
       </div>
   )
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
+import { useContext } from 'react';
+
+import { MoviesCategoriesContext } from '../MoviesCategoriesContext';
+
+import { MovieCard } from '../components/MovieCard';
+
 export function Content() {
-  // Complete aqui
+  const { movie, genreSelected } = useContext(MoviesCategoriesContext);
+  
+  const [ selectedGenre ] = genreSelected;
+  const [ movies ] = movie;
+  return(
+    <div className="container">
+          <header>
+            <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+          </header>
+
+          <main>
+            <div className="movies-list">
+              {movies.map(movie => (
+                <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+                ))}
+            </div>
+          </main>
+        </div>
+  )
 }
